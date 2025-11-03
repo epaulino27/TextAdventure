70d3562ea78436d06ca9ab6e90ad0d0531e83e79
iiitsanni <annikamaringer@online.de>
2025-11-03 14:01:45 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        WorldEnvironment singleton = new WorldEnvironment;
         Player player = new Player();
         Kraken enemy = new Kraken();
 
