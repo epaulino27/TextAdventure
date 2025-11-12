be4308ff3b9a710dcff31463279e9df5ff03a817
epaulino27 <143731967+epaulino27@users.noreply.github.com>
2025-11-11 23:00:45 -0500

    Main Demo, more clean

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        // Create player and enemy
+        // Create player
         Player player = new Player();
-        Kraken enemy = new Kraken();
 
-        // Create and attach LootSystem observer
+        // Define location, singleton
+        WorldEnvironment world = WorldEnvironment.getInstance();
+        world.setEnvironment("cave"); //if not set defaults to water
+
+        // Create enemy, factory
+        EnemyFactory factory = new EnemyFactory();
+        Fightable enemy = factory.Enemy(world.getEnvironment()); //takes water/forest/cave
+
+        // Enemy death check, loot observer attached
         LootSystem loot = new LootSystem();
         enemy.addDeathObserver(loot);
 
-        // Use Command pattern to execute attack
+        // Trigger/Simulate fight, command
         CommandInvoker Button = new CommandInvoker();
         CommandAttack Attack = new CommandAttack(player, enemy);
 
-        Button.setCurCommand(Attack);
-        Button.executeCommand();
-
-        Button.setCurCommand(Attack);
-        Button.executeCommand();
+        while(enemy.getStatus()){
+            Button.setCurCommand(Attack);
+            Button.executeCommand();
+            System.out.println();
+        }
     }
 }
